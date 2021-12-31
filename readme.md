e00bbada8fba3c2b19acc2403a6fe10d7930eada
rullyafrizal <rullyafrizal4@gmail.com>
2021-12-31 19:21:45 +0700

    finish material

--- readme.md
+++ readme.md
@@ -34,4 +34,37 @@
 - Context menganut konsep parent dan child
 - Artinya, ketika kita buat context, kita bisa buat juga child-nya
 - Parent context bisa memiliki banyak child, namun satu child hanya bisa punya satu parent
+- Child context juga bisa memiliki child lagi, disebut-nya sub-child
+- Semua fitur/data di parent context akan diwariskan ke semua child context
+- Semua fitur/data yang dipunyai child, belum tentu dimiliki oleh parent atau child lain
 - Konsep ini mirip inheritance di OOP
+- Jika ada suatu parent context di-cancel, maka semua child-nya pun akan ikut ter-cancel
+- Jika suatu child context dibatalkan, maka semua sub-child yang menjadi child dari child tersebut juga akan dibatalkan
+
+### Immutable
+- Context merupakan object yang immutable, artinya setelah Context dibuat, dia tidak bisa diubah lagi
+- Ketika kita menambahkan value ke dalam context, atau menambahkan pengaturan timeout dll, secara otomatis akan membentuk child context baru, bukan mengubah context tersebut
+
+## Context with Value
+- Pada saat awal membuat Context, dia tidak memiliki value
+- Kita bisa menambahkan value ke dalam context dengan menggunakan function **context.WithValue(parent, key, value)**
+- Value yang ditambahkan ke dalam context bersifat pair key-value
+- Saat kita menambah value ke context, secara otomatis akan tercipta child context baru, artinya original context tidak akan berubah
+
+## Context with Cancel
+- Context bisa dibatalkan dengan menggunakan function **context.WithCancel(parent)**
+- Context with Cancel biasa digunakan ketika kita menjalankan proses lain, dan kita ingin bisa memberi sinyal cancel ke proses tersebut
+- Biasanya proses ini berupa Goroutine yang berbeda, sehingga dengan mudah jika kita ingin membatalkan eksekusi Goroutine, kita tinggal kirim sinyal cancel ke context-nya
+- Perlu diingat, kita harus pastikan Goroutine yang menggunakan Context harus melakukan pengecekan terhadap Context-nya, jika tidak, tidak ada gunanya
+
+## Context with Timeout
+- Context bisa ditambahkan timeout dengan menggunakan function **context.WithTimeout(parent, duration)**
+- Dengan menggunakan pengaturan timeout, kita tidak perlu melakukan eksekusi cancel secara manual untuk membatalkan proses, cancel akan otomatis terjadi jika waktu timeout tercapai
+- Penggunaan timeout dengan context biasanya digunakan untuk menghindari infinite loop
+- Timeout dengan context juga sangat cocok ketika kita query ke database atau API, karena jika tidak kita hentikan, maka akan terlalu lama menunggu
+
+## Context with Deadline
+- Context bisa ditambahkan deadline dengan menggunakan function **context.WithDeadline(parent, time)**
+- Pengaturan deadline sedikit berbeda dengan timeout, jika waktu timeout adalah hitungan waktu terhadap waktu kita sekarang, maka deadline adalah waktu kapanpun kita mau, misal jam 12 siang besok
+- Deadline dengan context juga cocok untuk menghindari infinite loop
+  
--- src/context_test.go
+++ src/context_test.go
@@ -2,8 +2,11 @@ package src
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -12,4 +15,195 @@ func TestContext(t *testing.T) {
 
 	todo := context.TODO()
 	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	ctxA := context.Background()
+
+	ctxB := context.WithValue(ctxA, "A1", "rully")
+	ctxC := context.WithValue(ctxA, "A2", "zidan")
+
+	ctxD := context.WithValue(ctxB, "B1", "raufa")
+	ctxE := context.WithValue(ctxB, "B2", "khadafie")
+
+	ctxF := context.WithValue(ctxC, "C1", "mohammad")
+
+	fmt.Println(ctxA)
+	fmt.Println(ctxB)
+	fmt.Println(ctxC)
+	fmt.Println(ctxD)
+	fmt.Println(ctxE)
+	fmt.Println(ctxF)
+
+	// ambil value dari context dengan menggunakan key
+	fmt.Println(ctxB.Value("A1"))
+	fmt.Println(ctxC.Value("A2"))
+
+	// mengambil value dari parent context
+	fmt.Println(ctxD.Value("A1"))
+	fmt.Println(ctxF.Value("A2"))
+
+	// hasil nil karena context / parent context tidak memiliki value dengan key "B1"
+	fmt.Println(ctxF.Value("B1"))
+
+	// parent tidak bisa mengambil key dari child context, hasil nil
+	fmt.Println(ctxA.Value("A1"))
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+
+	// dengan leak
+	// destination := createCounter()
+
+	// tanpa leak
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := createCounter2(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+
+	// berguna untuk menghentikan proses sementara
+	time.Sleep(time.Second * 1)
+
+	// jika jumlah Goroutine 3, maka terjadi leak
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+}
+
+// Goroutine leak
+func createCounter() chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		var counter int = 1
+
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+
+	return destination
+}
+
+// Tanpa Goroutine leak
+func createCounter2(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		var counter int = 1
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	destination := createCounterTimeout(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+}
+
+func createCounterTimeout(ctx context.Context) chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+
+		counter := 1
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second))
+	defer cancel()
+
+	destination := createCounterTimeout(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	fmt.Printf("Total Goroutine : %d\n", runtime.NumGoroutine())
+}
+
+func operation1(ctx context.Context) error {
+	// Let's assume that this operation failed for some reason
+	// We use time.Sleep to simulate a resource intensive operation
+	time.Sleep(499 * time.Millisecond)
+	return errors.New("failed")
+}
+
+func operation2(ctx context.Context) {
+	// We use a similar pattern to the HTTP server
+	// that we saw in the earlier example
+	select {
+	case <-time.After(500 * time.Millisecond):
+		fmt.Println("done")
+	case <-ctx.Done():
+		fmt.Println("halted operation2")
+	}
+}
+
+func TestContext2(t *testing.T) {
+	// Create a new context, with its cancellation function
+	// from the original context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Run two operations: one in a different go routine
+	go func() {
+		err := operation1(ctx)
+		// If this operation returns an error
+		// cancel all operations using this context
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	// Run operation2 with the same context we use for operation1
+	// this below will be halted due to error in operation1
+	// because of the same context
+	operation2(ctx)
 }
